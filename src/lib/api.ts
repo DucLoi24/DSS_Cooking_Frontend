d69e922da24351858a84ed76536fdad8ebcf7247
DucLoi24 <ndlgaming2004@gmail.com>
2025-10-18 03:28:27 +0700

    ...

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,6 +1,6 @@
 import { useAuthStore } from './store';
 
-const BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL || 'http://127.0.0.1:8000';
+const BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
 
 const apiFetch = async (endpoint: string, options: RequestInit = {}) => {
     // Chúng ta lấy state trực tiếp, vì hàm này không phải là một hook
